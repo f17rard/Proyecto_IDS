9ea60fbe50ed6c1f9c2b310b5afbf4ce34891fa2
Gabriel Meneses <gabomendua@gmail.com>
2025-11-20 22:55:00 -0600

    Update operaciones.py

--- calculadora/operaciones.py
+++ calculadora/operaciones.py
@@ -1,16 +1,21 @@
+#aqui se definen las 4 funciones principales de la calculadora
 
 def suma_n(num1 = float, num2 = float):
+    """Funcion de suma que ocupa dos numeros"""
     resultado = num1+num2
     print(f"el resultado de la operación es: {resultado}")
 
 def resta_n(num1 = float, num2 = float):
+    """Funcion de resta que ocupa dos numeros"""
     resultado = num1-num2
     print(f"el resultado de la operación es: {resultado}")
 
 def multi_n(num1 = float, num2 = float):
+    """Funcion de multiplicación que ocupa dos numeros"""
     resultado = num1*num2
     print(f"el resultado de la operación es: {resultado}")
 
 def division_n(num1 = float, num2 = float):
+    """Funcion de división que ocupa dos numeros"""
     resultado = num1/num2
     print(f"el resultado de la operación es: {resultado}")
